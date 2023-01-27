1e1302ba6140eef0732d8c61cccaba20f66ad389
Christiano0407 <placebo0704@gmail.com>
2023-01-27 10:12:40 -0600

    check if winner

--- tictacGame/gameJs.js
+++ tictacGame/gameJs.js
@@ -52,7 +52,7 @@ startGame();
 function playerPlay() {
   const cells = document.querySelectorAll(`.cell`);
   cells.forEach((cell, i) => {
-    // >=Position
+    // >= Position
     const column = i % 3;
     const row = parseInt(i / 3);
 
@@ -69,4 +69,65 @@ function playerPlay() {
 //**? === PC Play */
 function pcPlay() {
   renderCurrentPlayer();
+  // === Timer
+  setTimeout(() => {
+    let played = false;
+    const options = checkIfCanWin();
+  }, 1500);
 }
+
+//** === CheckIfCanWin (A-1) */
+const checkIfCanWin = () => {
+  // >= Tip: Bi-dimension >= (Row = i && Column = j) <=
+  const arr = JSON.parse(JSON.stringify(board));
+
+  for (let i = 0; i < arr.length; i++) {
+    for (j = 0; j < arr.length; j++) {
+      // Validation === Detect Position "X" && "" && "O" ===
+      if (arr[i][j] === 'X') {
+        arr[i][j] = { value: 1, i: i, j: j };
+      }
+
+      if (arr[i][j] === '') {
+        arr[i][j] = { value: 0, i: i, j: j };
+      }
+
+      if (arr[i][j] === 'O') {
+        arr[i][j] = { value: -2, i: i, j: j };
+        // >= - + - = "-""
+      }
+    }
+  }
+
+  //*! === Conditions & Variables and Positions === (A-2) */
+  const p1 = arr[0][0];
+  const p2 = arr[0][1];
+  const p3 = arr[0][2];
+  const p4 = arr[1][0];
+  const p5 = arr[1][1];
+  const p6 = arr[1][2];
+  const p7 = arr[2][0];
+  const p8 = arr[2][1];
+  const p9 = arr[2][2];
+  //**! === Position Solutions Winner */
+  // >= "+" + "+" + "position"
+  const s1 = [p1, p2, p3];
+  const s2 = [p4, p5, p6];
+  const s3 = [p7, p8, p9];
+  const s4 = [p1, p4, p7];
+  const s5 = [p2, p5, p8];
+  const s6 = [p3, p6, p9];
+  const s7 = [p1, p5, p9];
+  const s8 = [p3, p5, p7];
+  //**! ===> Response  */
+  const response = [s1, s2, s3, s4, s5, s6, s7, s8].filter((line) => {
+    return (
+      line[0].value + line[1].value + line[2].value === 2 ||
+      line[0].value + line[1].value + line[2].value === -4
+    );
+  });
+  return response;
+};
+
+//** === checkIfWinner*/
+const checkIfWinner = () => {};
