e4190da9f4b920d88404f30066ae2b6a0516dade
Christiano0407 <placebo0704@gmail.com>
2023-01-29 13:08:47 -0600

    comments section part 01

--- sistComents/systemJs.js
+++ sistComents/systemJs.js
@@ -1,1 +1,62 @@
 //**TODO === === === ======================== System Comments ================================== === === ===  */
+//** ==== Variables ==== */
+//**? === Comments */
+let comments = [];
+const app = document.querySelector(`#app`);
+const commentsContainer = document.querySelector(`#comments-container`);
+//**? === Create DOM  */
+const inputContainer = document.createElement(`div`);
+const input = document.createElement(`input`);
+//** === */
+input.classList.add(`input`);
+input.addEventListener(`keydown`, (e) => {
+  handleEnter(e, null); // >= Call Other Function / Recursion (Recursividad) <=
+});
+//** === */
+commentsContainer.appendChild(inputContainer);
+inputContainer.appendChild(input);
+
+//** === new & Add Comment === (A) */
+const handleEnter = (e, current) => {
+  // === Validation ===
+  if (e.key === `Enter` && e.target.value !== ``) {
+    const newComment = {
+      text: e.target.value,
+      likes: 0,
+      response: [],
+    };
+
+    if (current === null) {
+      comments.unshift(newComment); // ==> Firs List
+    } else {
+      current.response.unshift(newComment);
+    }
+    // == Clear Interphase ===
+    e.target.value = ``;
+    commentsContainer.innerHTML = ``;
+    commentsContainer.appendChild(inputContainer);
+    //console.log(comments);
+    // === Render Comments
+    renderComments(comments, commentsContainer);
+  }
+};
+
+//** === Render Comments && Create Response Comment === (B) */
+const renderComments = (arr, parent) => {
+  arr.forEach((element) => {
+    // == Add Comment
+    const commentContainer = document.createElement(`div`);
+    commentContainer.classList.add(`comment-container`);
+    // === Response comment
+    const responseContainer = document.createElement(`div`);
+    responseContainer.classList.add(`response-container`);
+    // === Create => Other Option: Custom Element ===
+    const replyButton = document.createElement(`button`);
+    replyButton.classList.add(`btn-reply`);
+    const likeButton = document.createElement(`button`);
+    likeButton.classList.add(`btn-like`);
+
+    replyButton.textContent = `Reply`;
+    likeButton.textContent = `like`;
+  });
+};
