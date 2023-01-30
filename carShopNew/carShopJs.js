0579317477bd24c2f87642427d555f6d4dca9305
Christiano0407 <placebo0704@gmail.com>
2023-01-30 07:35:43 -0600

    shopping cart

---
+++ carShopNew/carShopJs.js
@@ -0,0 +1,7 @@
+//** ================================= ================================== === Car Shop === ===================================== ============================================== */
+//**TODO === === Variables */
+const app = document.querySelector(`#app`);
+const menu = document.querySelector(`#menu`);
+const shoppingCart = document.querySelector(`#shopping-cart-container`);
+const storeContainer = document.querySelector(`#store-container`);
+let shopping = [];
