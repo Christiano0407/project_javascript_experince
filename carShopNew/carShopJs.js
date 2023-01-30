48635e0e2ace2e3e61efaf8ffde5227251bc06dd
Christiano0407 <placebo0704@gmail.com>
2023-01-30 13:02:35 -0600

    render finished cart

--- carShopNew/carShopJs.js
+++ carShopNew/carShopJs.js
@@ -1,5 +1,5 @@
 //** ================================= ================== === Car Shop === ============ POO ================== ============================================== */
-//**TODO === === Variables */
+//**TODO === === === ======  Variables */
 const app = document.querySelector(`#app`);
 const menu = document.querySelector(`#menu`);
 const shoppingCartContainer = document.querySelector(
@@ -86,7 +86,7 @@ const shoppingCart = {
     },
     // === get
     get: (id) => {
-      const index = shoppingCart.findIndex((item) => item.id === id);
+      const index = shoppingCart.items.findIndex((item) => item.id === id);
       return index >= 0 ? shoppingCart.items[index] : null;
     },
     // === Get Total
@@ -97,6 +97,14 @@ const shoppingCart = {
       });
       return total;
     },
+    /*  getTotal: () => {
+      let total = 0;
+      shoppingCart.items.forEach((item) => {
+        const found = db.methods.find(item.id);
+        total += found.price * item.qty;
+      });
+      return total;
+    }, */
     // === Inventory Quantity
     hasInventory: (id, qty) => {
       return dataBase.items.find((item) => item.id === id).qty - qty >= 0; // == <= 0 / - 0 => Sorry Inventory ==
@@ -125,8 +133,105 @@ const renderStore = () => {
         `;
   });
   storeContainer.innerHTML = html.join('');
+  // == Buttons
+  document.querySelectorAll(`.item .actions .add`).forEach((btn) => {
+    btn.addEventListener(`click`, (e) => {
+      e.preventDefault();
+      const id = parseInt(btn.getAttribute(`data-id`)); // = Shopping / Transform a Number
+      const item = dataBase.methods.find(id); // = Data Base
+      // === Validation & Add
+      if (item && item.qty - 1 > 0) {
+        // => Add a Shopping Cart
+        shoppingCart.methods.add(id, 1);
+        console.log(shoppingCart);
+        renderShoppingCart();
+      } else {
+        console.log('Not Exist more Product && Not more in Inventory');
+      }
+    });
+  });
 };
 renderStore();
+//** === === === Render Shopping Cart (D) */
+//**? = Add Shopping Cart Product = */
+function renderShoppingCart() {
+  const html = shoppingCart.items.map((product) => {
+    const dataProduct = dataBase.methods.find(product.id); // == Call or Return DataBase Item
+    return `
+     <div class="item">
+           <div class="title">${dataProduct.title}</div>
+           <div class="price">Price: ${numberToCurrency(
+             dataProduct.price
+           )}</div>
+           <div class="qty">${product.qty} Units</div>
+           <div class="subtotal">Subtotal: ${numberToCurrency(
+             product.qty * dataProduct.price
+           )}</div>
+           <div class="actions">
+              <button class="addOne" data-id="${dataProduct.id}">+</button>
+              <button class="removeOne" data-id="${dataProduct.id}">-</button>
+           </div>
+        </div>
+     `;
+  });
+  // === Buttons && Custom Elements Add HTML ===
+  const closeButton = `
+    <div class="cart-header" >
+        <button class="bClose" id="bClose">Close</button>
+    </div>
+  `;
+  // == Finished Shop
+  const purchaseButton =
+    shoppingCart.items.length > 0
+      ? `<div class="cart-actions">
+    <button id="bPurchase">Terminar compra</button>
+  </div>`
+      : '';
+
+  const total = shoppingCart.methods.getTotal();
+
+  const totalContainer = `<div class="total">Total: ${numberToCurrency(
+    total
+  )}</div>`;
+
+  shoppingCartContainer.innerHTML =
+    closeButton + html.join(``) + totalContainer + purchaseButton;
+
+  // === Buttons Event Add & Remove
+  document.querySelectorAll(`.addOne`).forEach((button) => {
+    button.addEventListener(`click`, (e) => {
+      e.preventDefault();
+      const id = parseInt(button.getAttribute(`data-id`));
+      shoppingCart.methods.add(id, 1);
+      renderShoppingCart();
+    });
+  });
+  document.querySelectorAll(`.removeOne`).forEach((button) => {
+    button.addEventListener(`click`, (e) => {
+      e.preventDefault();
+      const id = parseInt(button.getAttribute(`data-id`));
+      shoppingCart.methods.remove(id, 1);
+      renderShoppingCart();
+    });
+  });
+  shoppingCartContainer.classList.remove(`hide`);
+  shoppingCartContainer.classList.add(`show`);
+  document.querySelector(`#bClose`).addEventListener(`click`, (e) => {
+    // === Add & Remove ===
+    shoppingCartContainer.classList.remove(`hide`);
+    shoppingCartContainer.classList.add(`show`);
+    //shoppingCartContainer.classList.toggle(`hide`);
+  });
+  // => Depending bPurchaseButton Add or Not
+  const bPurchase = document.querySelector(`#bPurchase`);
+  if (bPurchase) {
+    bPurchase.addEventListener(`click`, (e) => {
+      shoppingCart.methods.purchase();
+      renderStore();
+      renderShoppingCart();
+    });
+  }
+}
 
 //** === Price In Dollars === (C.1) */
 // ==> Intl (Format / API) => U.S Dollars
