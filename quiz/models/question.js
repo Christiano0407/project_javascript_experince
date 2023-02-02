be1a38e0ce6e6f2f872502a1d2983bdcee17f82b
Christiano0407 <placebo0704@gmail.com>
2023-02-02 14:57:05 -0600

    apart 01

--- quiz/models/question.js
+++ quiz/models/question.js
@@ -1,5 +1,16 @@
 //** * === Questions === * ==== POO Class ==== ADD => Methods, Object & Functions ====  */
 //**TODO === Class === */
-class Questions {
-  constructor() {}
+export class Questions {
+  constructor(text, choice, answer) {
+    this.text = text;
+    this.choice = choice;
+    this.answer = answer;
+  }
+  // === Method
+  correctAnswer(choice) {
+    return choice === this.choice;
+  }
 }
+/* const people = new Questions('Hello Text', 'New Choice', true);
+console.log(people);
+console.log(people.correctAnswer()); */
--- quiz/quizApp.js
+++ quiz/quizApp.js
@@ -2,6 +2,6 @@
 const quiz = document.querySelector(`#quiz`);
 const question = document.querySelector(`#question`);
 const choice = document.querySelector(`#choices`);
-
+//** === Import */
 import { data } from './data/data.js';
-console.log(data);
+import { Questions } from './models/question.js';
