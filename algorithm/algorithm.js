7606bb3c8162c8ba5a647d29241b4ef6466acafb
Christiano0407 <placebo0704@gmail.com>
2023-02-02 08:20:43 -0600

    algorithms part 01

---
+++ algorithm/algorithm.js
@@ -0,0 +1,17 @@
+//** === === === ================================ Algorithm =================================== === === === */
+//**! === === */
+let numbers = [5, 10, 15, 30, 45, 60, 8, 6];
+let numberMaximum = 0;
+let position = numbers.length;
+
+function callNumber() {
+  for (let i = 0; i < position; i++) {
+    if (numberMaximum < numbers[i]) {
+      numberMaximum = numbers[i];
+      console.log(numberMaximum);
+    }
+  }
+}
+callNumber();
+
+//**! === ===  */
