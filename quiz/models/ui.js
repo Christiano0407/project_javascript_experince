ee3eded2b379fd333f90026faca0a6a1ac14f394
Christiano0407 <placebo0704@gmail.com>
2023-02-03 12:10:23 -0600

    finished

--- quiz/models/ui.js
+++ quiz/models/ui.js
@@ -17,14 +17,32 @@ export class UI {
    */
   showChoices(choices, callback) {
     const choiceContainer = document.querySelector(`#choices`);
+
+    choiceContainer.innerHTML = '';
+
     for (let i = 0; i < choices.length; i++) {
       const button = document.createElement(`button`);
       button.innerText = choices[i]; // = choice
       button.classList.add(`btn-choices`);
 
-      button.addEventListener('click', () => callback());
+      button.addEventListener('click', () => callback(choices[i])); // => CurrentChoice
 
       choiceContainer.append(button);
     }
+  }
+
+  // == Show
+  showScores(score) {
+    const gameOverHTML = `
+   <h1>Result</h1>
+   <h2 id="score">Your Score: ${score}</h2>
+   `;
+    const element = document.querySelector(`#quiz`);
+    element.innerHTML = gameOverHTML;
+  }
+
+  showProgress(currentIndex, total) {
+    const element = document.querySelector(`#progress`);
+    element.innerHTML = `Question ${currentIndex} of ${total} `;
   }
 }
--- quiz/quizApp.js
+++ quiz/quizApp.js
@@ -1,5 +1,5 @@
 //** ============== POO (Programming Oriented Object)============ QUIZ Application ======================================= */
-const quiz = document.querySelector(`#quiz`);
+//@ts-check
 //** === Import */
 //import { data } from './data/data.js';
 //import { Questions } from './models/question.js';
@@ -7,11 +7,24 @@ import { newQuestions } from './data/questions.js';
 import { Quiz } from './models/Quiz.js';
 import { UI } from './models/ui.js';
 
+// == Render == (Questions & Answer)
+const renderPage = (quiz, ui) => {
+  if (quiz.isEnded()) {
+    ui.showScores(quiz.score);
+  } else {
+    console.log(quiz);
+    ui.showQuestions(quiz.getQuestionIndex().question);
+    ui.showProgress(quiz.questionIndex + 1, quiz.newQuestions);
+    ui.showChoices(quiz.getQuestionIndex().choice, (currentChoice) => {
+      quiz.guess(currentChoice);
+      renderPage(quiz, ui);
+    }); // === Add Callback ===
+  }
+};
+
 function main() {
   const quiz = new Quiz(newQuestions);
   const ui = new UI();
-  console.log(quiz);
-  ui.showQuestions(quiz.getQuestionIndex().question);
-  ui.showChoices(quiz.getQuestionIndex().choice, () => console.log('Buttons')); // === Add Callback ===
+  renderPage(quiz, ui);
 }
 main();
