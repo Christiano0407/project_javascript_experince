576a67762e107145ec2eb775e5c0ff20877e1c51
Christiano0407 <placebo0704@gmail.com>
2023-02-05 15:53:07 -0600

    new algorithm

--- algorithm/algorithm.js
+++ algorithm/algorithm.js
@@ -18,4 +18,13 @@ function callNumber() {
 let maximum = Math.max(5, 10, 15, 30, 45, 60, 8, 6);
 //console.log(maximum);
 
-//**! === Practice One: Performance Time and Space ===  */
+//**! === Practice One: Performance Time and Space === (01) */
+//** === Análisis Asintótico === (02) */
+function printNumber(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      console.log(i, j);
+    }
+  }
+}
+printNumber(3); // 3 * 3 => 9 (Al cuadrado)
