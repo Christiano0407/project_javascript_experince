4607664b333a35c7bc8f6b735da5adbf3880fc90
Christiano0407 <placebo0704@gmail.com>
2023-03-21 09:15:08 -0600

    new finished clock

--- clock/clockApp.js
+++ clock/clockApp.js
@@ -7,21 +7,21 @@ const secondHand = document.getElementById(`idSecond`);
 
 //** ======== Events ======== */
 
-const newDates = new Date(); // Get Date
-
 const addDates = () => {
+  const newDates = new Date(); // Get Date
+
   const second = newDates.getSeconds(); // Method Second
   const getSecondsDegree = (second / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${getSecondsDegree}deg)`;
   //console.log(getSeconds);
 
   const minutes = newDates.getMinutes();
-  const getMinutesDegree = (minutes / 60) * 360 + 90;
+  const getMinutesDegree = (minutes / 60) * 360 + (second / 60) * 6 + 90;
   minuteHand.style.transform = `rotate(${getMinutesDegree}deg)`;
   //console.log(minutes);
 
   const hour = newDates.getHours();
-  const getHourDegree = (hour / 12) * 360 + 90;
+  const getHourDegree = (hour / 12) * 360 + (minutes / 60) * 30 + 90;
   hourHand.style.transform = `rotate(${getHourDegree}deg)`;
   //console.log(hour);
 };
