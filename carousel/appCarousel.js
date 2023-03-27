3cf4c1a02df65a7fed396118a1cbc9bcf61407f9
Christiano0407 <placebo0704@gmail.com>
2023-03-27 10:22:48 -0600

    finished all project images

--- carousel/appCarousel.js
+++ carousel/appCarousel.js
@@ -1,2 +1,22 @@
 //** =========== === Carousel === ============ */
 console.log('Hello World');
+//** ==== ====== ===== */
+const panels = document.querySelectorAll(`.panel`);
+
+function openImages() {
+  console.log('Open Images');
+  this.classList.toggle(`open`);
+}
+
+function imageTransform(e) {
+  //this.classList.toggle(`open`);
+  console.log(e.propertyName);
+  if (e.propertyName.includes(`flex-grow`)) {
+    this.classList.toggle(`open-active`);
+  }
+}
+
+panels.forEach((image) => image.addEventListener('click', openImages));
+panels.forEach((image) =>
+  image.addEventListener('transitionend', imageTransform)
+);
