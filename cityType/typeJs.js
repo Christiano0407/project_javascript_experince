d950fbde9219be621b0698a1e30faf96581ee0a3
Christiano0407 <placebo0704@gmail.com>
2023-03-31 12:54:24 -0600

    new api challenge with json - challenge

---
+++ cityType/typeJs.js
@@ -0,0 +1,5 @@
+//** ==== === Type City === API REST Javascript === ====  */
+//**! === API REST / JSON / EndPoint === */
+const endpoint =
+  'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+console.log(endpoint);
